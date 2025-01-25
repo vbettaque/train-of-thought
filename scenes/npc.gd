67df45fe1d625749d4d8dc47789dfdaff12a25fa
vladislav manoylo <10890153+vmanoylo@users.noreply.github.com>
2025-01-25 12:35:45 -0500

    train car scene
    - stock background
    - player walking
    - npc with dialog box

---
+++ scenes/npc.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if has_overlapping_areas():
+		$Dialog.visible = true
+	else:
+		$Dialog.visible = false
