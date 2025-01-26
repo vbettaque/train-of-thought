4a5e7eac7d3cdfdd9e993744b3fb762e01972853
Valerie Bettaque <v.l.bettaque@gmail.com>
2025-01-26 12:51:20 -0500

    added basic title screen

---
+++ scenes/title_screen/title_screen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@export_file("*.tscn") var next_scene_path: String
+
+@onready var play_button: Button = $MarginContainer/CenterContainer/VBoxContainer/PlayButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play_button.pressed.connect(_on_button_pressed)
+
+
+func _on_button_pressed() -> void:
+	if next_scene_path:
+		get_tree().change_scene_to_file(next_scene_path)
